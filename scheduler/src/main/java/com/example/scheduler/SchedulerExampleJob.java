79cef897da91a2f5643863bd5b102533f84ecfd2
minhyeok <omh34658981@gmail.com>
2023-07-16 17:31:57 +0900

    config spring scheduler thread pool size

--- scheduler/src/main/java/com/example/scheduler/SchedulerExampleJob.java
+++ scheduler/src/main/java/com/example/scheduler/SchedulerExampleJob.java
@@ -14,29 +14,36 @@ import static com.example.scheduler.DateTimeFormatterConstant.DATE_TIME_FORMATTE
 public class SchedulerExampleJob {
     private static final String CRON_EXPRESSION_EXAMPLE = "30 * * * * *";
 
-    @Scheduled(cron = CRON_EXPRESSION_EXAMPLE)
+//    @Scheduled(cron = CRON_EXPRESSION_EXAMPLE)
     public void cron() {
         log.info(">>>>>시작 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
     }
 
     @Scheduled(fixedRate = 3000)
-    public void fixedRate(){
-        log.info(">>>>>시작 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
+    public void fixedRate1() throws InterruptedException {
+        log.info(">>>>>fixedRate1 시작 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
+        Thread.sleep(5000);
     }
 
-    @Scheduled(fixedDelay = 3000)
+    @Scheduled(fixedRate = 3000)
+    public void fixedRate2() throws InterruptedException {
+        log.info(">>>>>fixedRate2 시작 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
+        Thread.sleep(5000);
+    }
+
+//    @Scheduled(fixedDelay = 3000)
     public void fixedDelay() throws InterruptedException {
         log.info(">>>>>시작 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
         Thread.sleep(5000);
         log.info(">>>>>종료 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
     }
 
-    @Scheduled(fixedRate = 2000, initialDelay = 3000)
+//    @Scheduled(fixedRate = 2000, initialDelay = 3000)
     public void initialDelayWithFixedRate() {
         log.info(">>>>>실행 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
     }
 
-    @Scheduled(fixedDelay = 2000, initialDelay = 3000)
+//    @Scheduled(fixedDelay = 2000, initialDelay = 3000)
     public void initialDelayWithFixedDelay() {
         log.info(">>>>>실행 시간 {}", LocalDateTime.now().format(DATE_TIME_FORMATTER));
     }
---
+++ scheduler/src/main/java/com/example/scheduler/config/SchedulerConfig.java
@@ -0,0 +1,21 @@
+package com.example.scheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+    private static final int POOL_SIZE = 5;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("쓰레드-");
+        threadPoolTaskScheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
